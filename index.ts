51a4204d96862ecc500963fff8240d807894ae79
nono2224 <88888931+nono2224@users.noreply.github.com>
2025-03-06 01:03:17 +0900

    change

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import DeserializeRRSMessage from "./src/DeserializeRRSMessage";
 
-const message = new DeserializeRRSMessage("G/HRUe5haAtgDMAAMHWA", 2);
+// AK_SPEAKコマンドには，送信したエージェントのエンティティIDと，送信したStep(Time)，送信に使用したチャンネル，送信した生データの4つが含まれます
+const entityId: number = 11111111;
+const step: number = 99;
+const channel: number = 1;
+const rawData: string = "G5W+XU5haAtgDMAAMn3A";
+
+// 生データとチャンネルと渡すと，メッセージの内容を解読してくれます
+const message = new DeserializeRRSMessage(rawData, channel);
 
 console.log(message);
